d68f82679954c1933be2714dbffb0c45b2353dbd
w3ttheonlyone <emfabian.meyer1337@web.de/em>
2015-06-16 16:13:31 +0200

    configuring cmake

---
+++ CMakeFiles/fiber.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/fiber.dir/src/main.cpp.obj"
+  "bin/fiber.pdb"
+  "bin/fiber.exe"
+  "bin/fiber.exe.manifest"
+  "libfiber.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/fiber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
+#include <iostream>
+using namespace std;
 int launchFiber(){
+	cout << "Hello Fiber!"<<endl;
 	return 0;
 }
 int main(){
