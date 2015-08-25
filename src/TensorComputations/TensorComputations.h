7a467cc71d9fd1a37baaab5b44f1d6c82ec8249f
Valentin Kraft <valentin.kraft@online.de>
2015-08-25 11:58:30 +0200

    Added EigenvectorComputation method

--- src/TensorComputations/TensorComputations.h
+++ src/TensorComputations/TensorComputations.h
@@ -9,14 +9,23 @@
 #include "vtkPointData.h"
 #include "vtkDoubleArray.h"
 #include "vtkDenseArray.h"
-#include "vtkGenericCellIterator.h"
+//#include "vtkGenericCellIterator.h"
+
+#include <vtkVersion.h>
+#include "vtkSmartPointer.h"
+#include "vtkDoubleArray.h"
+#include "vtkMultiBlockDataSet.h"
+#include "vtkPCAStatistics.h"
+#include "vtkStringArray.h"
+#include "vtkTable.h"
 
 class TensorComputations
 {
 public:
 	TensorComputations();
     vtkSmartPointer<vtkDenseArray<double> > GetTensorsFromNIFTI(vtkSmartPointer<vtkNIFTIImageReader> niftiReader, int dim[]);
+	void GetEigenvectorsFromTensor(vtkSmartPointer<vtkDenseArray<double>> tensors, vtkSmartPointer<vtkDoubleArray> *eigenvector1, vtkSmartPointer<vtkDoubleArray> *eigenvector2, vtkSmartPointer<vtkDoubleArray> *eigenvector3);
 	~TensorComputations();
 };
 
-#endif // BOXVIEW_H
+#endif
